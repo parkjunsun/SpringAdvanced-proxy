d606bfe8e9e459c1e1b2b2f484825863869b3ca0
parkjunsun <qkrwnstns52@naver.com>
2022-03-19 21:01:22 +0900

    2022.03.19 commit

---
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,36 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.*;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        Component component = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(component);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component component = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(component);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+
+        client.execute();
+    }
+}
